eb3d2ea7164c65cdd804ee4a861018cea13d1206
Yogansh Tembhare <118890931+Yogansh11@users.noreply.github.com>
2022-11-26 21:42:59 +0530

    Add files via upload

---
+++ Project_1.js
@@ -0,0 +1,23 @@
+var count = 1;
+function red() {
+
+	if (count == 1) {
+		document.getElementById("Red").style.background = 'red';
+		document.getElementById("Yellow").style.background = 'red';
+		document.getElementById("Green").style.background = 'red';
+	}
+}
+function yellow() {
+	if (count == 1) {
+		document.getElementById("Red").style.background = 'yellow';
+		document.getElementById("Yellow").style.background = 'yellow';
+		document.getElementById("Green").style.background = 'yellow';
+	}
+}
+function green() {
+	if (count == 1) {
+		document.getElementById("Red").style.background = 'green';
+		document.getElementById("Yellow").style.background = 'green';
+		document.getElementById("Green").style.background = 'green';
+	}
+}
